904cf4e754a1b45ebab981a5b16056ddd1fff821
grosje <grosje@f203-11.e-im2ag.ujf-grenoble.fr>
2016-01-06 09:43:58 +0100

    Controle structure

--- Controle.cpp
+++ Controle.cpp
@@ -6,7 +6,30 @@
 using namespace std;
 
 
-    void Controle::afficher(){
+    void Controle::affichageImage(IplImage img){
         cout << "beubeu" << endl;
     }
 
+    IplImage Controle::ouvertureImage(string chemin){
+        return null;
+    }
+
+    IplImage Controle::filtrage(IplImage img){ //ici creation nouvelle image
+        return null;
+    }
+
+    IplImage Controle::amelioration(IplImage img){ //ici creation nouvelle image
+        return null;
+    }
+
+    IplImage Controle::crop(IplImage img, CvScalar pixel1, CvScalar pixel2){
+        return null;
+    }
+
+    void Controle::redimension(IplImage img, string option, int l, int h){
+        return null;
+    }
+
+    IplImage Controle::afficherHistogramme(CvScalar pixel, string optionCoul){
+        return null;
+    }
--- Controle.h
+++ Controle.h
@@ -1,9 +1,16 @@
 #ifndef CONTROLE_H
 #define CONTROLE_H
 class Controle{
+    //private:
+
     public:
-    void afficher();
-    private:
+    IplImage ouvertureImage(string chemin);
+    void affichageImage(IplImage img);
+    IplImage filtrage(IplImage img); //ici creation nouvelle image
+    IplImage amelioration(IplImage img); //ici creation nouvelle image
+    IplImage crop(IplImage img, CvScalar pixel1, CvScalar pixel2);
+    void redimension(IplImage img, string option, int l, int h);
+    IplImage afficherHistogramme(CvScalar pixel, string optionCoul);
 };
 #endif // CONTROLE_H
 
